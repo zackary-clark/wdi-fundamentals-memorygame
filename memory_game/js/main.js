a0445828c3e3ccd57fd65014960f9d1aa4925a6e
Zack Clark <zackary.clark.2@us.af.mil>
2018-09-06 10:22:30 -0400

    Unit 10 assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,30 @@
+function Card(rank, suit, image) {
+	this.rank = rank;
+	this.suit = suit;
+	this.cardImage = image;
+}
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+new Card("queen","hearts","images/queen-of-hearts.png"),
+new Card("queen","diamonds","images/queen-of-diamonds.png"),
+new Card("king","hearts","images/king-of-hearts.png"),
+new Card("king","diamonds","images/king-of-diamonds.png")
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
